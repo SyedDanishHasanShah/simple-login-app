984eb5db410741f60b4af91ac2061a9f3b2cfb0c
SyedDanishHasanShah <danish.hasan@arbisoft.com>
2021-08-05 10:51:00 +0500

    Added a modal window instead of using alert(). Used the convention of adding classes to change CSS rather than changing it directly through JavaScript. Added event listeners so modal window closes on pressing Escape key and by clicking anywhere outside of the modal window.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const submitButton = document.querySelector('.submit-button');
-const modalWindow = document.querySelector('.main__modal');
+const modalWindow = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const modalHeading = document.querySelector('.modal__heading');
 const modalClose = document.querySelector('.modal__close');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
@@ -10,15 +12,21 @@ submitButton.addEventListener('click', function() {
     const isValidEmail = validateEmail(emailValue);
     const isValidPassword = validatePassword(passwordValue);
     if (validateEmail(emailValue) && validatePassword(passwordValue)) {
-        alert('Login Successful');
+        // alert('Login Successful');
+        modalHeading.innerHTML = "Login successful!";
+        openModal();
+
     }
     else {
-        alert('Invalid input(s)! Please try again!');
+        // alert('Invalid input(s)! Please try again!');
+        modalHeading.innerHTML = 'Login failed!';
+        openModal();
         if (!isValidEmail) {
-            email.style.border = "3px solid red";
+            email.classList.add('input--invalid');
         }
         if (!isValidPassword) {
-            password.style.border = "3px solid red";
+            console.log("hello");
+            password.classList.add('input--invalid');
         }
     }
     email.value = '';
@@ -29,18 +37,35 @@ email.addEventListener('keydown', () => changeBorderToNormal(email));
 
 password.addEventListener('keydown', () => changeBorderToNormal(password));
 
+modalClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && !modalHeading.classList.contains('hidden')) closeModal();
+});
+
 
 function changeBorderToNormal(element) {
-    if (element.style.border === '3px solid red') element.style.border = "2px solid black";
-}
+    if (element.classList.contains('input--invalid')) element.classList.remove('input--invalid');
+};
 
 function validateEmail(email) {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
-}
+};
 
 function validatePassword(password) {
     // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
-}
+};
+
+function openModal() {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+function closeModal() {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
